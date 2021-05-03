3837ea07f37967da24ece3e5c13fc027c418fd26
Richard Houltz <richard@houltz.se>
2021-05-03 07:56:30 +0200

    Changed layout of mobile navigation button

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,8 +67,8 @@
     var $mobile_nav = $('.nav-menu').clone().prop({
       class: 'mobile-nav d-lg-none'
     });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('#header').append($mobile_nav);
+    $('#header').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
